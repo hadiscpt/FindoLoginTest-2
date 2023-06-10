12729d33e737d0dcc923d5f23a70e1f5ef097a10
hadiscpt <mhadisucipto21@gmail.com>
2023-06-10 15:12:24 +0700

    Initial Commit

---
+++ FindoLoginTest/FindoLoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindoLoginTestApp.swift
+//  FindoLoginTest
+//
+//  Created by Muhammad Hadi Sucipto on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FindoLoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
